d2f6be0f3fbfa255c721b52300f065741827882f
liuzoneze <592090207@qq.com>
2019-03-26 15:24:44 +0800

    SHARED lib

--- MathFunctions/MathFunctions.h
+++ MathFunctions/MathFunctions.h
@@ -1,1 +1,13 @@
-double mysqrt(double x);
+
+//#ifdef WIN32
+
+#ifdef MathFunctions_EXPORTS
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif
+
+//#endif
+
+// STATIC lib 无需以上声明及EXPORT
+EXPORT double mysqrt(double x);
