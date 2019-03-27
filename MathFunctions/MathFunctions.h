acd46c30edf48033084e147c672e9865f76d7bc7
liuzoneze <592090207@qq.com>
2019-03-27 08:39:03 +0800

    test platform

--- MathFunctions/MathFunctions.h
+++ MathFunctions/MathFunctions.h
@@ -1,13 +1,16 @@
 ﻿
-//#ifdef WIN32
+//如果是windows platform
+#ifdef WIN32
 //Typical pattern: <target-name>_EXPORTS
 #ifdef MathFunctions_EXPORTS
 #define EXPORT __declspec(dllexport)
 #else
 #define EXPORT __declspec(dllimport)
 #endif
-
-//#endif
+//其他platform
+#else
+#define EXPORT
+#endif
 
 // STATIC lib 无需以上声明及EXPORT
 EXPORT double mysqrt(double x);
